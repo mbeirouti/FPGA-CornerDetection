b44bfbd079cb3e87b7f8b1cb73cc35f6dcbd7a29
mcataford <c.marcandre@gmail.com>
2017-03-26 19:21:06 -0400

    Documented.

--- TriIntMemory.vhdl
+++ TriIntMemory.vhdl
@@ -5,8 +5,14 @@ use ieee.std_logic_1164.all;
 entity TriIntMemory is
 
 	port (
+		-- Input
+		-- Clock signal
 		clock: in std_logic;
+		-- x,y,xy squares signals
 		yIn, xIn, xyIn: in integer;
+		
+		-- Output
+		-- x,y,xy square signals
 		yOut,xOut,xyOut: out integer
 	);
 	
@@ -14,6 +20,7 @@ end entity;
 
 architecture TriIntMemory_Impl of TriIntMemory is
 
+-- Intermediate, memory.
 signal xMem,yMem,xyMem: integer := 0;
 
 begin
@@ -24,10 +31,12 @@ begin
 	
 		if rising_edge(clock) then
 		
+			-- Assigning output.
 			yOut <= yMem;
 			xOut <= xMem;
 			xyOut <= xyMem;
 			
+			-- Updating memory.
 			yMem <= yIn;
 			xMem <= xIn;
 			xyMem <= xyIn;
