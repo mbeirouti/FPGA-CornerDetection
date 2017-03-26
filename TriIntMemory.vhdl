9c4e4767d16a5a5997e9827fe536a325051da749
mcataford <c.marcandre@gmail.com>
2017-03-26 19:05:33 -0400

    TriIntMemory stub, internal for the HCD

---
+++ TriIntMemory.vhdl
@@ -0,0 +1,16 @@
+library IEEE;
+
+entity TriIntMemory is
+
+	port (
+		yIn, xIn, xyIn: in integer;
+		yOut,xOut,xyOut: out integer
+	);
+	
+end entity;
+
+architecture TriIntMemory_Impl of TriIntMemory is
+
+begin
+
+end architecture;
