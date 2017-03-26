71a629bed71d1d9b5f145806f0938ae3e1f15f5c
mcataford <c.marcandre@gmail.com>
2017-03-26 19:07:32 -0400

    HCD internal memory filled.

--- TriIntMemory.vhdl
+++ TriIntMemory.vhdl
@@ -3,6 +3,7 @@ library IEEE;
 entity TriIntMemory is
 
 	port (
+		clock: in std_logic;
 		yIn, xIn, xyIn: in integer;
 		yOut,xOut,xyOut: out integer
 	);
@@ -11,6 +12,26 @@ end entity;
 
 architecture TriIntMemory_Impl of TriIntMemory is
 
+signal xMem,yMem,xyMem: integer := 0;
+
 begin
+
+	process
+	
+	begin
+	
+		if rising_edge(clock) then
+		
+			yOut <= yMem;
+			xOut <= xMem;
+			xyOut <= xyMem;
+			
+			yMem <= yIn;
+			xMem <= xIn;
+			xyMem <= xyIn;
+		
+		end if;
+	
+	end process;
 
 end architecture;
