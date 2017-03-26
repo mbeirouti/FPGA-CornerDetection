c70addb4334b387ec2a24c4be3c97a4eb3dee280
mcataford <c.marcandre@gmail.com>
2017-03-26 01:03:30 -0400

    Corrected and commented unit.

--- ThresholdingUnit.vhdl
+++ ThresholdingUnit.vhdl
@@ -5,8 +5,14 @@ use ieee.std_logic_1164.all;
 entity ThresholdingUnit is
 	
 	port(
+		-- Input
+		-- Threshold value
 		thresh: in integer;
+		-- Confidence score
 		confidence: in integer;
+		
+		-- Output
+		-- Decision taken
 		decision: out std_logic
 	);
 
@@ -15,6 +21,9 @@ end entity;
 architecture ThresholdingUnit_Impl of ThresholdingUnit is
 
 begin
+	
+	-- Since decision-making doesn't require sync., it is implemented as
+	-- a combinational circuit.
 	
 	decision <= '1' when confidence >= thresh else
 				'0';
