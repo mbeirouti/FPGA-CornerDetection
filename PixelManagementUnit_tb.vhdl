0910a4ab986462a45b4ebea50a6b490427863183
mcataford <c.marcandre@gmail.com>
2017-03-26 00:25:09 -0400

    Testbench stub.

---
+++ PixelManagementUnit_tb.vhdl
@@ -0,0 +1,12 @@
+library IEEE;
+
+use ieee.std_logic_1164.all;
+
+entity PixelManagementUnit_tb is
+end entity;
+
+architecture PixelManagementUnit_tst of PixelManagementUnit_tb is
+
+begin
+
+end architecture;
