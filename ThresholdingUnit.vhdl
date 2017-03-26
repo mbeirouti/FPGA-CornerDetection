fa9ebaf3c47478f7e1038e54fc9fc0cdced6db3a
mcataford <c.marcandre@gmail.com>
2017-03-26 00:54:46 -0400

    Comparator impl.

--- ThresholdingUnit.vhdl
+++ ThresholdingUnit.vhdl
@@ -7,8 +7,16 @@ entity ThresholdingUnit is
 	port(
 		thresh: in integer;
 		confidence: in integer;
-		
 		decision: out std_logic
 	);
 
 end entity;
+
+architecture ThresholdingUnit_Impl of ThresholdingUnit is
+
+begin
+	
+	decision <= '1' when confidence >= thresh else
+				'0';
+
+end architecture;
