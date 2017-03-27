46a4af1bd1c0cf9e91a5c8aaa1c28bb12bd97e6b
mcataford <c.marcandre@gmail.com>
2017-03-27 01:52:38 -0400

    Clean up. Removed unused package.

--- VHDL-Project/yGradient.vhd
+++ VHDL-Project/yGradient.vhd
@@ -3,46 +3,35 @@ library IEEE;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-use std.textio.all;
-
 entity yGradient is
 	
 	-- Takes as input a matrix of 9 values and outputs the gradient in the Y-Direction based on the Sobel operator
 	-- NOTE: Can be optimized further
 	port(
 		clk : in std_logic;
-		x11, x12, x13, x21, x22, x23, x31, x32, x33 : in unsigned(7 downto 0);
-		gradientInY : out unsigned(7 downto 0));
+		x11, x12, x13, x21, x22, x23, x31, x32, x33 : in integer;
+		gradientInY : out integer
+	);
 
 end yGradient;
 
 
 architecture implementation of yGradient is
-	
-	
-	
-	begin
 
+	begin
 	
 	SobelOperator : process(clk)
 
 	begin
 		
 		-- On rising edge
-		if(clk'EVENT and clk = '1') then
+		if rising_edge(clk) then
 			
 			-- Calculate gradient using Sobel operator
 			gradientInY <= x11 + 2*x12 + x13 - x31 - 2*x32 - x33;
-			
-			
-		
+
 		end if;
 	
 	end process SobelOperator;
 	
-	
-	
-	
-
-
 end implementation;
