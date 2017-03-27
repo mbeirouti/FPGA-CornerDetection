4dbb433a597c86d5dbcedfb86ba7253faa69f7f5
Matthew Beirouti <matthew.beirouti@mail.mcgill.ca>
2017-03-27 01:00:30 -0400

    Added Test Bench for Squares

--- VHDL-Project/squares.vhd
+++ VHDL-Project/squares.vhd
@@ -10,11 +10,11 @@ entity squares is
 
 	port(
 		clk : in std_logic;
-		xGrad : in std_logic_vector(7 downto 0);
-		yGrad : in std_logic_vector(7 downto 0);
-		xGradSq : out std_logic_vector(15 downto 0);
-		yGradSq: out std_logic_vector(15 downto 0);
-		xyGrad : out std_logic_vector(15 downto 0));
+		xGrad : in unsigned(7 downto 0);
+		yGrad : in unsigned(7 downto 0);
+		xGradSq : out unsigned(15 downto 0);
+		yGradSq: out unsigned(15 downto 0);
+		xyGrad : out unsigned(15 downto 0));
 
 end squares;
 
@@ -26,10 +26,20 @@ architecture implementation of squares is
 	begin
 
 
+	SquareOperator : process(clk)
 	
+		begin
+		
+			if(clk'EVENT and clk = '1') then
 	
+				xGradSq <= xGrad * xGrad;
+				yGradSq <= yGrad * yGrad;
+				xyGrad <= xGrad * yGrad;
 	
-	
+			end if;
+			
+			
+	end process SquareOperator;
 
 
 end implementation;
--- VHDL-Project/xGradient.vhd
+++ VHDL-Project/xGradient.vhd
@@ -11,8 +11,8 @@ entity xGradient is
 	-- NOTE: Can be optimized further
 	port(
 		clk : in std_logic;
-		x11, x12, x13, x21, x22, x23, x31, x32, x33 : in std_logic_vector(7 downto 0);
-		gradientInX : out std_logic_vector(7 downto 0));
+		x11, x12, x13, x21, x22, x23, x31, x32, x33 : in unsigned(7 downto 0);
+		gradientInX : out unsigned(7 downto 0));
 
 end xGradient;
 
@@ -24,33 +24,35 @@ architecture implementation of xGradient is
 	begin
 
 	
---	SobelOperator : process(clk)
---	
---		-- Intermediate variable used to ensure the operation returns a result between 0 and 255
---		variable intermediate : integer range -1021 to 1021;
---	
---	begin
---		
---		-- On rising edge
---		if(clk'EVENT and clk = '1') then
---			
---			-- Calculate gradient using Sobel operator
---			-- NOTE: Not sure if this is the most efficient way to do integer subtraction but needed to get it done
+	SobelOperator : process(clk)
+	
+		-- Intermediate variable used to ensure the operation returns a result between 0 and 255
+		variable intermediate : integer range -1021 to 1021;
+	
+	begin
+		
+		-- On rising edge
+		if(clk'EVENT and clk = '1') then
+			
+			-- Calculate gradient using Sobel operator
+			-- NOTE: Not sure if this is the most efficient way to do integer subtraction but needed to get it done
 --			intermediate := to_integer(signed(x11))+2*to_integer(signed(x21))+to_integer(signed(x31)) - to_integer(signed(x13))-2*to_integer(signed(x23))-to_integer(signed(x33));
 --			
 --			-- If result is out of bounds put it in bounds
-----			if (intermediate > 255) then
-----				intermediate := 255;
-----			elsif (intermediate < 0) then
-----				intermediate := 0;
-----			end if;
---			
---			-- Output value
+--			if (intermediate > 255) then
+--				intermediate := 255;
+--			elsif (intermediate < 0) then
+--				intermediate := 0;
+--			end if;
+			
+			-- Output value
 --			gradientInX <= std_logic_vector(to_signed(intermediate, 8));
---		
---		end if;
---	
---	end process SobelOperator;
+			
+			gradientInX <= x11 + 2*x21 + x31 - x13 - 2*x23 - x33;
+		
+		end if;
+	
+	end process SobelOperator;
 	
 	
 	gradientInX <= x11;
