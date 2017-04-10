396d009ee54ef2ed909324247b4a867898d79624
mcataford <c.marcandre@gmail.com>
2017-04-09 23:12:58 -0400

    Final mods to allow proper reconstruction.

--- SupportScripts/imagePostprocessing.py
+++ SupportScripts/imagePostprocessing.py
@@ -20,29 +20,15 @@ format = input("Format: ")
 #Open file
 file = open(filename,"rb")
 
-print("Reading file...")
+image = Image.new("L",(800,600))
 
-#Parse file header (upper 8 bytes, little endian) into width/height words.
-width = int.from_bytes(file.read(4),byteorder="little")
-height = int.from_bytes(file.read(4),byteorder="little")
+for i in range(800):
+	print("Column {} of {}".format(i,800))
+	for j in range(600):
+		image.putpixel((i,j),int.from_bytes(file.read(1),byteorder="little"))
 
-#Create container for image
-image = Image.new(mode="L",size=(width,height))
-
-#Reconstructing the pixel matrix from bin file bytes.
-for i in range(width):
-	for j in range(height):
-		pixel = int.from_bytes(file.read(1),byteorder="little")
-		image.putpixel((i,j),pixel)
-		
-#Closing image stream
 file.close()
-
-targetFilename = "reconstructed.{}".format(format)
-
-#Saving to final file.
-image.save(targetFilename)
-
-print("All done! Generated {}".format(targetFilename))
+		
+image.save("reconstructed.jpg")
 
 
