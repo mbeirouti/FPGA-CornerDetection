6bd91998cfed5d433f3f8a48ab8272c808c5ae4b
Matthew Beirouti <matthew.beirouti@mail.mcgill.ca>
2017-04-10 00:13:16 -0400

    Added gaussian operator test assitant script
    Gauss works perfectly

--- SupportScripts/gaussTestHelper.c
+++ SupportScripts/gaussTestHelper.c
@@ -7,23 +7,24 @@
 int main () {
 
 	int gtrdn;
-	int array[6];
+	int array[9];
 	int out;
 	while (1){
 
-		for(gtrdn = 0; gtrdn < 6; gtrdn++){
+		for(gtrdn = 0; gtrdn < 9; gtrdn++){
 
 			int in;
 			scanf(" %x",&in);
 
-			if (in == 999){
+			if (in == 0x999){
 				return 0;
 			}
 
 			array[gtrdn] = in;
 		}
 
-		out = array[0] + 2*array[1] + array[2] - array[3] - 2*array[4] -array[5];
+		out = array[0] + 2*array[1] + array[2] + 2*array[3] + 4*array[4] + 2*array[5] + array[6] + 2*array[7] + array[8];
+		out = out / 16;
 
 		if (out > 255){
 			out = 255;
