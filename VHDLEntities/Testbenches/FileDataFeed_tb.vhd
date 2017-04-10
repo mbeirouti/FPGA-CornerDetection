35aeaaa2e690a9c115e927eccdee6da22c9c248b
mcataford <c.marcandre@gmail.com>
2017-04-10 00:58:24 -0400

    Modified tb to test port input, tested ok.

--- VHDLEntities/Testbenches/FileDataFeed_tb.vhd
+++ VHDLEntities/Testbenches/FileDataFeed_tb.vhd
@@ -19,15 +19,20 @@ architecture FileDataFeed_tst of FileDataFeed_tb is
 	signal feedReady: std_logic := '0';
 	signal imgWidth,imgHeight: std_logic_vector(31 downto 0) := (others => '0');
 	signal pixelRow1,pixelRow2,pixelRow3: std_logic_vector(23 downto 0) := (others => '0');
+	signal pixelIn: std_logic_vector(7 downto 0);
 	
 	component FileDataFeed
+	
+		generic(
+			from_file: boolean := false
+		);
 	
 		port(
 			CLOCK: in std_logic;
 			sendPatch: in std_logic;
+			pixelIn: in std_logic_vector(7 downto 0);
 			
 			feedReady: out std_logic;
-			imgWidth,imgHeight : out std_logic_vector(31 downto 0) := (others => 'Z');
 			pixelRow1,pixelRow2,pixelRow3: out std_logic_vector(23 downto 0) := (others => 'Z')
 		);
 		
@@ -35,7 +40,7 @@ architecture FileDataFeed_tst of FileDataFeed_tb is
 
 begin
 
-	FDF: FileDataFeed port map(CLOCK, sendPatch,feedReady, imgWidth, imgHeight, pixelRow1, pixelRow2, pixelRow3);
+	FDF: FileDataFeed port map(CLOCK, sendPatch,pixelIn,feedReady, pixelRow1, pixelRow2, pixelRow3);
 
 	CLK: process
 	
@@ -54,11 +59,7 @@ begin
 	
 		for i in 0 to 479999 loop
 		
-			sendPatch <= '1';
-			
-			wait for 1 * CLK_PERIOD;
-			
-			sendPatch <= '0';
+			pixelIn <= std_logic_vector(to_unsigned(255,8));
 			
 			wait for 1 * CLK_PERIOD;
 		
