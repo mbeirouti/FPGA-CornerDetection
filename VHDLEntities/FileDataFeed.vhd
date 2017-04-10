4e1df23a934736dfbb08ab22ea8e3c42d78ca6c5
mcataford <c.marcandre@gmail.com>
2017-04-10 00:58:05 -0400

    Ip/op memory, file and/or port input.

--- VHDLEntities/FileDataFeed.vhd
+++ VHDLEntities/FileDataFeed.vhd
@@ -8,13 +8,18 @@ library STD;
 use std.textio.all;
 
 entity FileDataFeed is
+	
+	generic(
+		from_file: boolean := true
+	);
+
 
 	port(
 		CLOCK: in std_logic;
 		sendPatch: in std_logic;
+		pixelIn: in std_logic_vector(7 downto 0);
 	
 		feedReady: out std_logic := '0';
-		imgWidth,imgHeight : out std_logic_vector(31 downto 0) := (others => 'Z');
 		pixelRow1,pixelRow2,pixelRow3: out std_logic_vector(23 downto 0) := (others => 'Z')
 	);
 
@@ -27,10 +32,13 @@ architecture FileDataFeed_Impl of FileDataFeed is
 	file inFile: char_file_t;
 	file outFile: char_file_t;
 	
+	constant imageWidth: integer := 800;
+	constant imageHeight: integer := 600;
+	
 	--Internal memory
 	--Fit for a 800x600 image
-	type memory is array (0 to 800*600-1) of std_logic_vector(7 downto 0);
-	signal imageBuffer: memory;
+	type memory is array (0 to imageHeight+1, 0 to imageWidth+1) of std_logic_vector(7 downto 0);
+	signal imageBuffer: memory := (others => (others => (others => '0')));
 
 begin
 
@@ -42,12 +50,11 @@ begin
 		variable outByte: character;
 		variable currentByte: integer := 0;
 		
-		variable imageWidth,imageHeight : std_logic_vector(31 downto 0):= (others => 'Z');
-		variable iWidth,iHeight,totalSize: integer := 0;
+		variable totalSize: integer := 0;
 		
-		variable patchLocationX,patchLocationY: integer := 0;
+		variable locX,locY: integer := 1;
 		
-		variable currentState, nextState : integer range 0 to 4 := 0;
+		variable currentState, nextState : integer range 0 to 6 := 0;
 
 	begin
 
@@ -57,38 +64,21 @@ begin
 		--State behaviour
 		case currentState is
 		
-			--State 0:
-			--Buffer initialization
-			--Source file is read and organized into the internal memory
-			--	so it's ready to be fed to the system.
-			
 			when 0 =>
+				if from_file then
+					nextState := 1;
+				else
+					nextState := 2;
+				end if;
+			
+			when 1 =>
 			
 				--Open input file
 				file_open(inFile, "processedImageData.bin", read_mode);
 
 				--File is written as a hex blob, no newlines.
 
-				--Read the first 4 bytes (word) of the file, image height.
-				for i in 0 to 3 loop
-				
-					read(inFile,inByte);
-					imageWidth(7+i*8 downto 0+i*8) := std_logic_vector(to_unsigned(character'pos(inByte),8));		
-				
-				end loop;
-
-				--Read second 4 bytes(word) of the file, image width.
-				
-				for i in 0 to 3 loop
-				
-					read(inFile,inByte);
-					imageHeight(7+i*8 downto 0+i*8) := std_logic_vector(to_unsigned(character'pos(inByte),8));
-				
-				end loop;
-				
-				iWidth := to_integer(unsigned(imageWidth));
-				iHeight := to_integer(unsigned(imageHeight));
-				totalSize := iWidth * iHeight;
+				totalSize := imageWidth * imageHeight;
 
 				--Calculated data segment size.
 				report "Calculated size: "& integer'image(totalSize) & " bytes of image data.";
@@ -96,73 +86,101 @@ begin
 				while not endfile(inFile) loop
 					
 					read(inFile,inByte);
-				
-					imageBuffer(currentByte) <= std_logic_vector(to_unsigned(character'pos(inByte),8));
+						
+					imageBuffer(locX,locY) <= std_logic_vector(to_unsigned(character'pos(inByte),8));
 					currentByte := currentByte + 1;
+							
+					if locX < imageHeight then
+						locX := locX + 1;
+					else
+						locX := 1;
+						locY := locY + 1;
+					end if;
 
 				end loop;
 				
-				--Setting output
-				imgWidth <= imageWidth;
-				imgHeight <= imageHeight;
-				
 				--Signaling that the feed is ready.
 				feedReady <= '1';
+				locX := 0;
+				locY := 0;
 				
-				nextState := 1;
-				
-			when 1 =>
+				nextState := 3;
+
+			when 2 =>
+					
+					imageBuffer(locX,locY) <= pixelIn;
+					
+					if locX = imageHeight and locY = imageWidth then
+						locX := 0;
+						locY := 0;
+						
+						nextState := 5;
+					end if;
+					
+					if locX < imageHeight then
+						locX := locX + 1;
+					else
+						locX := 1;
+						locY := locY + 1;
+					end if;
+					
+					report integer'image(locY);
+			
+			when 3 =>
 			
 				if sendPatch = '1' then
 				
-					pixelRow1 <= imageBuffer(patchLocationX+800*patchLocationY) & imageBuffer(patchLocationX+1+800*patchLocationY) & imageBuffer(patchLocationX+2+800*patchLocationY);
+					pixelRow1 <= imageBuffer(locX,locY) & imageBuffer(locX+1,locY) & imageBuffer(locX+2,locY);
 					
-					pixelRow2 <= imageBuffer(patchLocationX+800*(patchLocationY+1)) & imageBuffer(patchLocationX+1+800*(patchLocationY+1)) & imageBuffer(patchLocationX+2+800*(patchLocationY+1));
+					pixelRow2 <= imageBuffer(locX,locY+1) & imageBuffer(locX+1,locY+1) & imageBuffer(locX+2,locY+1);
 					
-					pixelRow3 <= imageBuffer(patchLocationX+800*(patchLocationY+2)) & imageBuffer(patchLocationX+1+800*(patchLocationY+2)) & imageBuffer(patchLocationX+2+800*(patchLocationY+2));
+					pixelRow3 <= imageBuffer(locX,locY+2) & imageBuffer(locX+1,locY+2) & imageBuffer(locX+2,locY+2);
 				
-					nextState := 2;
+					nextState := 4;
 				
 				end if;
 				
-			when 2 =>
+			when 4 =>
 			
 				if sendPatch = '1' then
 				
-					if (patchLocationX + 1) < 800-2 then
-						patchLocationX := patchLocationX + 1;
+					if (locX + 1) < imageWidth-1 then
+						locX := locX + 1;
 						
 					else
 					
 						report "Row done.";
-					
-						patchLocationX := 0;
 						
-						if patchLocationY < 600-2 then
-							patchLocationY := patchLocationY + 1;
+						nextState := 3;
+					
+						locX := 0;
+						
+						if locY < imageHeight-1 then
+							locY := locY + 1;
 						else
 							feedReady <= '0';
+							nextState := 5;
 						end if;
 						
 					end if;
 				
-					nextState := 1;
-				
 				end if;
 			
-			when 3 =>
+			when 5 =>
 				
 				file_open(outFile, "M1_dump.bin", write_mode);
 				
-				for i in 0 to 800*600-1 loop
+				for i in 1 to imageWidth loop
+					for j in 1 to imageHeight loop
 				
-					write(outFile,character'val(to_integer(unsigned(imageBuffer(i)))));
+						write(outFile,character'val(to_integer(unsigned(imageBuffer(j,i)))));
 				
+					end loop;
 				end loop;
 				
-				nextState := 4;
+				nextState := 6;
 				
-			when 4 =>
+			when 6 =>
 			
 				report "NOP.";
 				
